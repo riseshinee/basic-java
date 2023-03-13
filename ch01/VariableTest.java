a842079d544467d9d44de8558799824fed16d0c3
seungheee <lsh3431@gmail.com>
2023-03-13 23:59:51 +0900

    ✨ number type

---
+++ ch01/VariableTest.java
@@ -0,0 +1,12 @@
+package ch01;
+
+public class VariableTest {
+    public static void main(String[] args){
+        byte bNum = 127;
+
+        System.out.println(bNum);
+
+        long lNum = 12345678900L;
+        System.out.println(lNum);
+    }
+}
