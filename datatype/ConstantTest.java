e1c0235055ce26021a25a346c4ca89f6883a3a9c
seungheee <lsh3431@gmail.com>
2023-03-19 23:49:28 +0900

    ✨ Constant type
    상수 형변환 연습

--- datatype/ConstantTest.java
+++ datatype/ConstantTest.java
@@ -4,9 +4,9 @@ public class ConstantTest {
     
     public static void main(String[] args){
 
-        final int MAX_NUM = 100; //변하지 않는 상수 선언
+        final int MAX_NUM = 100; //상수 선언
         final int MIN_NUM;
-        MIN_NUM = 0;
+        MIN_NUM = 0; //재선언
 
         System.out.println(MAX_NUM);
         System.out.println(MIN_NUM);
---
+++ datatype/TypeConversionTest.java
@@ -0,0 +1,22 @@
+package datatype;
+
+public class TypeConversionTest {
+    
+    public static void main(String[] args){
+        byte bNum = 125;
+        int iNum = 225;
+        //byte jNum = iNum; // error
+        byte jNum = (byte)iNum;//강제로 형변환 함
+
+        System.out.println(jNum);
+
+        double dNum = 1.2;
+        float fNum = 0.9F;
+        int xNum1 = (int)dNum + (int)fNum;
+        int xNum2 = (int)(dNum + fNum);
+
+        System.out.println(xNum1);
+        System.out.println(xNum2);
+    }
+
+}
