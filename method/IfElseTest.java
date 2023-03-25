720d96626877e2f7400d76649ed6546eb4dffa83
seungheee <lsh3431@gmail.com>
2023-03-25 17:41:02 +0900

    ✨ if, else test
    if, else 조건문 연습

---
+++ method/IfElseTest.java
@@ -0,0 +1,16 @@
+package method;
+
+public class IfElseTest {
+
+    public static void main(String[] args){
+        int age = 7;
+
+        if(age >= 8){
+            System.out.println("학교에 다님");
+        }else{
+            System.out.println("학교에 안다님");
+        }
+
+    }
+    
+}
